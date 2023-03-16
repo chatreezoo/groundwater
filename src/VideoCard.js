cf117af24c152aac80d62e07eeb211917fdce27b
MakoMori-37 <oun_kup@hotmail.com>
2023-03-16 22:32:39 +0700

    style video card

--- src/VideoCard.js
+++ src/VideoCard.js
@@ -3,68 +3,56 @@ import VideoRow from "./VideoRow";
 import "./VideoCard.css";
 
 function VideoCard() {
+  // image, title, explain 
+  const data = [
+    {
+      title : 'วิชาที่ 1 ความรู้เบี้องต้นด้านน้ำบาดาล',
+      image:"https://www.technologychaoban.com/wp-content/uploads/2018/01/%E0%B8%AA%E0%B8%81%E0%B8%B9%E0%B9%8A%E0%B8%9B-%E0%B8%99%E0%B9%89%E0%B8%B3%E0%B8%9A%E0%B8%B2%E0%B8%94%E0%B8%B2%E0%B8%A5.jpg",
+      explain:"วัฏจักของน้ำและความหมายของน้ำบาดาลคุรสมบัติทางชลศาสตร์ของหินอุ้มน้ำ"
+    },
+    {
+      title:"วิชาที่ 2 ความรู้เบี้องต้นด้านน้ำบาดาล",
+      image:"https://www.technologychaoban.com/wp-content/uploads/2018/01/%E0%B8%AA%E0%B8%81%E0%B8%B9%E0%B9%8A%E0%B8%9B-%E0%B8%99%E0%B9%89%E0%B8%B3%E0%B8%9A%E0%B8%B2%E0%B8%94%E0%B8%B2%E0%B8%A5.jpg",
+      explain:"วัฏจักของน้ำและความหมายของน้ำบาดาลคุรสมบัติทางชลศาสตร์ของหินอุ้มน้ำ"
+    },
+    {
+      title:"วิชาที่ 3 ความรู้เบี้องต้นด้านน้ำบาดาล",
+      image:"https://www.technologychaoban.com/wp-content/uploads/2018/01/%E0%B8%AA%E0%B8%81%E0%B8%B9%E0%B9%8A%E0%B8%9B-%E0%B8%99%E0%B9%89%E0%B8%B3%E0%B8%9A%E0%B8%B2%E0%B8%94%E0%B8%B2%E0%B8%A5.jpg",
+      explain:"วัฏจักของน้ำและความหมายของน้ำบาดาลคุรสมบัติทางชลศาสตร์ของหินอุ้มน้ำ"
+    },
+    {
+      title:"วิชาที่ 4 ความรู้เบี้องต้นด้านน้ำบาดาล",
+      image:"https://www.technologychaoban.com/wp-content/uploads/2018/01/%E0%B8%AA%E0%B8%81%E0%B8%B9%E0%B9%8A%E0%B8%9B-%E0%B8%99%E0%B9%89%E0%B8%B3%E0%B8%9A%E0%B8%B2%E0%B8%94%E0%B8%B2%E0%B8%A5.jpg",
+      explain:"วัฏจักของน้ำและความหมายของน้ำบาดาลคุรสมบัติทางชลศาสตร์ของหินอุ้มน้ำ"
+    },
+    {
+      title:"วิชาที่ 5 ความรู้เบี้องต้นด้านน้ำบาดาล",
+      image:"https://www.technologychaoban.com/wp-content/uploads/2018/01/%E0%B8%AA%E0%B8%81%E0%B8%B9%E0%B9%8A%E0%B8%9B-%E0%B8%99%E0%B9%89%E0%B8%B3%E0%B8%9A%E0%B8%B2%E0%B8%94%E0%B8%B2%E0%B8%A5.jpg",
+      explain:"วัฏจักของน้ำและความหมายของน้ำบาดาลคุรสมบัติทางชลศาสตร์ของหินอุ้มน้ำ"
+    },
+    {
+      title:"วิชาที่ 6 ความรู้เบี้องต้นด้านน้ำบาดาล",
+      image:"https://www.technologychaoban.com/wp-content/uploads/2018/01/%E0%B8%AA%E0%B8%81%E0%B8%B9%E0%B9%8A%E0%B8%9B-%E0%B8%99%E0%B9%89%E0%B8%B3%E0%B8%9A%E0%B8%B2%E0%B8%94%E0%B8%B2%E0%B8%A5.jpg",
+      explain:"วัฏจักของน้ำและความหมายของน้ำบาดาลคุรสมบัติทางชลศาสตร์ของหินอุ้มน้ำ"
+    },
+    {
+      title:"วิชาที่ 7 ความรู้เบี้องต้นด้านน้ำบาดาล",
+      image:"https://www.technologychaoban.com/wp-content/uploads/2018/01/%E0%B8%AA%E0%B8%81%E0%B8%B9%E0%B9%8A%E0%B8%9B-%E0%B8%99%E0%B9%89%E0%B8%B3%E0%B8%9A%E0%B8%B2%E0%B8%94%E0%B8%B2%E0%B8%A5.jpg",
+      explain:"วัฏจักของน้ำและความหมายของน้ำบาดาลคุรสมบัติทางชลศาสตร์ของหินอุ้มน้ำ"
+    },
+    {
+      title:"วิชาที่ 8 ความรู้เบี้องต้นด้านน้ำบาดาล",
+      image:"https://www.technologychaoban.com/wp-content/uploads/2018/01/%E0%B8%AA%E0%B8%81%E0%B8%B9%E0%B9%8A%E0%B8%9B-%E0%B8%99%E0%B9%89%E0%B8%B3%E0%B8%9A%E0%B8%B2%E0%B8%94%E0%B8%B2%E0%B8%A5.jpg",
+      explain:"วัฏจักของน้ำและความหมายของน้ำบาดาลคุรสมบัติทางชลศาสตร์ของหินอุ้มน้ำ"
+    }
+
+  ]
   return (
     <div className="VideoCard">
       <div className="Card">
-        <div>
-          <VideoRow
-            title="วิชาที่ 1 ความรู้เบี้องต้นด้านน้ำบาดาล"
-            image="https://www.technologychaoban.com/wp-content/uploads/2018/01/%E0%B8%AA%E0%B8%81%E0%B8%B9%E0%B9%8A%E0%B8%9B-%E0%B8%99%E0%B9%89%E0%B8%B3%E0%B8%9A%E0%B8%B2%E0%B8%94%E0%B8%B2%E0%B8%A5.jpg"
-            explain="วัฏจักของน้ำและความหมายของน้ำบาดาลคุรสมบัติทางชลศาสตร์ของหินอุ้มน้ำ"
-          />
-        </div>
-        <div className="Card_2">
-          <VideoRow
-            title="วิชาที่ 2 ความรู้เบี้องต้นด้านน้ำบาดาล"
-            image="https://www.technologychaoban.com/wp-content/uploads/2018/01/%E0%B8%AA%E0%B8%81%E0%B8%B9%E0%B9%8A%E0%B8%9B-%E0%B8%99%E0%B9%89%E0%B8%B3%E0%B8%9A%E0%B8%B2%E0%B8%94%E0%B8%B2%E0%B8%A5.jpg"
-            explain="วัฏจักของน้ำและความหมายของน้ำบาดาลคุรสมบัติทางชลศาสตร์ของหินอุ้มน้ำ"
-          />
-        </div>
-        <div className="Card_2">
-          <VideoRow
-            title="วิชาที่ 3 ความรู้เบี้องต้นด้านน้ำบาดาล"
-            image="https://www.technologychaoban.com/wp-content/uploads/2018/01/%E0%B8%AA%E0%B8%81%E0%B8%B9%E0%B9%8A%E0%B8%9B-%E0%B8%99%E0%B9%89%E0%B8%B3%E0%B8%9A%E0%B8%B2%E0%B8%94%E0%B8%B2%E0%B8%A5.jpg"
-            explain="วัฏจักของน้ำและความหมายของน้ำบาดาลคุรสมบัติทางชลศาสตร์ของหินอุ้มน้ำ"
-          />
-        </div>
-        <div className="Card_2">
-          <VideoRow
-            title="วิชาที่ 4 ความรู้เบี้องต้นด้านน้ำบาดาล"
-            image="https://www.technologychaoban.com/wp-content/uploads/2018/01/%E0%B8%AA%E0%B8%81%E0%B8%B9%E0%B9%8A%E0%B8%9B-%E0%B8%99%E0%B9%89%E0%B8%B3%E0%B8%9A%E0%B8%B2%E0%B8%94%E0%B8%B2%E0%B8%A5.jpg"
-            explain="วัฏจักของน้ำและความหมายของน้ำบาดาลคุรสมบัติทางชลศาสตร์ของหินอุ้มน้ำ"
-          />
-        </div>
-      </div>
-
-      <div className="Caed__low" >
-        <div className="Card_01">
-          <VideoRow
-            title="วิชาที่ 5 ความรู้เบี้องต้นด้านน้ำบาดาล"
-            image="https://www.technologychaoban.com/wp-content/uploads/2018/01/%E0%B8%AA%E0%B8%81%E0%B8%B9%E0%B9%8A%E0%B8%9B-%E0%B8%99%E0%B9%89%E0%B8%B3%E0%B8%9A%E0%B8%B2%E0%B8%94%E0%B8%B2%E0%B8%A5.jpg"
-            explain="วัฏจักของน้ำและความหมายของน้ำบาดาลคุรสมบัติทางชลศาสตร์ของหินอุ้มน้ำ"
-          />
-        </div>
-        <div className="Card_1">
-          <VideoRow
-            title="วิชาที่ 6 ความรู้เบี้องต้นด้านน้ำบาดาล"
-            image="https://www.technologychaoban.com/wp-content/uploads/2018/01/%E0%B8%AA%E0%B8%81%E0%B8%B9%E0%B9%8A%E0%B8%9B-%E0%B8%99%E0%B9%89%E0%B8%B3%E0%B8%9A%E0%B8%B2%E0%B8%94%E0%B8%B2%E0%B8%A5.jpg"
-            explain="วัฏจักของน้ำและความหมายของน้ำบาดาลคุรสมบัติทางชลศาสตร์ของหินอุ้มน้ำ"
-          />
-        </div>
-        <div className="Card_1">
-          <VideoRow
-            title="วิชาที่ 7 ความรู้เบี้องต้นด้านน้ำบาดาล"
-            image="https://www.technologychaoban.com/wp-content/uploads/2018/01/%E0%B8%AA%E0%B8%81%E0%B8%B9%E0%B9%8A%E0%B8%9B-%E0%B8%99%E0%B9%89%E0%B8%B3%E0%B8%9A%E0%B8%B2%E0%B8%94%E0%B8%B2%E0%B8%A5.jpg"
-            explain="วัฏจักของน้ำและความหมายของน้ำบาดาลคุรสมบัติทางชลศาสตร์ของหินอุ้มน้ำ"
-          />
-        </div>
-        <div className="Card_1">
-          <VideoRow
-            title="วิชาที่ 8 ความรู้เบี้องต้นด้านน้ำบาดาล"
-            image="https://www.technologychaoban.com/wp-content/uploads/2018/01/%E0%B8%AA%E0%B8%81%E0%B8%B9%E0%B9%8A%E0%B8%9B-%E0%B8%99%E0%B9%89%E0%B8%B3%E0%B8%9A%E0%B8%B2%E0%B8%94%E0%B8%B2%E0%B8%A5.jpg"
-            explain="วัฏจักของน้ำและความหมายของน้ำบาดาลคุรสมบัติทางชลศาสตร์ของหินอุ้มน้ำ"
-          />
-        </div>
+        {data?.map((item, index) => ((
+          <VideoRow key={index} image={item.image} title={item.title} explain={item.explain}   />
+        )))}
       </div>
     </div>
   );
